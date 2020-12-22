a771e78ca8d762b55b6342b50a2f32607a31e96c
Justin <jwilson051@student.butte.edu>
2020-12-22 09:32:13 +0000

    adding final

--- labs/lab_jquery/script.js
+++ labs/lab_jquery/script.js
@@ -1,2 +1,91 @@
 /* For Testing Purposes */
+
+
+//1) Get the HTML of the first paragraph on the page and store in the variable named x
+var x = $("p").first();
+
+
+//2) Get rid of (remove from the DOM) all images
+
 $('img').remove();
+
+
+//3) Append to the 5th paragraph on the page a <b> element with the text "I'm Fifth!"
+
+
+//4) Give .news-article(s) the class of "tomato"
+
+
+//5) Make all paragraphs on the page have a text color of 'skyblue'
+
+
+//6) Give the <body> an id attribute with the value of 'reversed'
+
+
+//7) When you click on a <h2> inside an .article it should toggle between showing/hiding the image inside the same .article
+
+
+//8) When the mouse enters a .news-article, make it transparent
+   //- You MUST use fadeTo()
+
+
+//9) Set the height of the first .article to be 1000px
+
+
+//10) Set the HTML of the last paragraph on the page to be:
+    //"<b>This is the last paragraph</b>"
+
+
+//11) Add a new paragraph between the <h3> and <p> of the second .news-article.
+    //The content of the paragraph should be: "In the Middle"
+    //- You MUST use .after()
+
+
+//12) Make a duplicate of the second .article and put it as the first .article on the page
+    //- You MUST use .prependTo() and .clone()
+
+
+//13) When you click on any paragraph on the page it should toggle between having and not having the 'bold' class
+
+
+//14) Get the float value of the #news section and store it in a variable named x.
+
+
+//15) When you hover over a paragraph it's class should become 'tomato'.
+    //When you are done hovering the class should go away
+
+
+//16) Get the width of the <body> and store in a variable named x
+
+
+//17) Add a new .article (<section>) as the first article on the page. It must have the article class and contain a <h2> with the text "First"
+    //- You MUST use .before()
+
+
+//18) Set the Text of all even paragraphs on the page (the 2nd, 4th, etc) to be "I'm even!"
+
+
+//19) Replace all images with a paragraph containing the text "Where'd the images go?"
+
+
+//20) When you click on a .article, fade it out 'slow'ly
+
+
+//21) Get the width of the #news section, including padding, border and margin and store it in a variable named x
+
+
+//22) Get the value of the id attribute of the first section on the page
+
+
+//23) Get the Text of all of the .news-article(s) on the page and store in a variable named x.
+
+
+//24) Make all paragraphs on the page have a text color of 'white', a background color of 'black' and padding of '1em'.
+    //- You must use an object literal
+
+
+//25) When you click on an image, toggle (via a slide animation) all sibling paragraphs.
+
+
+//26) Remove the 'bold' class from all elements that have it
+
